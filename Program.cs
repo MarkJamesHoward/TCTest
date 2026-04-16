e95b700f7c8761a97760512766b25d5ebca7adc4
Mark Howard <markjameshoward@hotmail.co.uk>
2026-04-16 19:42:29 +1200

    force again

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Hello, World!");
 System.Console.WriteLine("Hello, World! again");
 System.Console.WriteLine("Hello, World! third");
+System.Console.WriteLine("Hello, World! third");
